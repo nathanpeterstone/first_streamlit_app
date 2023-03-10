4e59fc2513b22b5fccec800e3e020b8d9133c7a2
nathanpeterstone <nathan@nathanstone.co.uk>
2023-03-10 12:31:37 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,12 @@ def insert_row_snowflake(new_fruit):
     return f"Thanks for adding {new_fruit}"
     
 
+if streamlit.button('get fruit load list'):
+  my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)    
+    
+    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
 if streamlit.button('add a fruit to the list'):
